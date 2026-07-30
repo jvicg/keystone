d8c30666648ce71de6623bbbcc3e537696a41854
jcorreag <jcorreag@pm.me>
2026-07-30 19:11:56 +0200

    add: Add project metadata

---
+++ keystone/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+"""
+Automatic installation of Arch Linux using Ansible as backend tool.
+"""
---
+++ keystone/__version__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+Package metadata.
+"""
+
+__title__ = "keystone"
+__version__ = "0.1.0"
